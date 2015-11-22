1a88a717463302c1a0fdaed742d83ed8cf9cc03d
alexmady <alexmady@gmail.com>
2015-11-22 23:33:54 +0000

    - media queries & sprite refined methodology working

--- www/app/dashboard/components/course/course.js
+++ www/app/dashboard/components/course/course.js
@@ -23,18 +23,32 @@ courseModule
             });
     }])
 
-    .controller('CourseCtrl', [ '$scope', '$state', function ($scope, $state) {
+    .controller('CourseCtrl', [ '$scope', '$state', '$window',
+        function ($scope, $state, $window) {
+
+        console.log('CourseController.......');
 
         $scope.go = function (goTo) {
             $state.go(goTo);
         };
 
+        $scope.debug = function(){
+            console.log('debug');
+            console.log('Window width: ' + $window.innerWidth);
+            console.log('Window Height: ' + $window.innerHeight);
+            console.log('Sprite: ' + $scope.spritePNGFile);
+            console.log('Sprite Data File' + $scope.spriteDataFileName);
+            console.log('Data');
+            console.log($scope.items);
+        };
 
         $scope.play = function (){
+            console.log('play');
             TweenBoxy.play();
         };
 
         $scope.pause = function (){
+            console.log('pause');
             TweenBoxy.pause();
         };
 
@@ -44,13 +58,61 @@ courseModule
         };
 
 
-        var boxyObj1 = { counter: 0};
+
+        $scope.spritePNGFile = '../img/sprite-' + $window.innerWidth + 'x' + $window.innerHeight + '.png';
+
+
+        // e.g. '../img/sprite-375x667.json'.
+        // this must match corresponding sprite png file.
+        $scope.spriteDataFileName = '../img/sprite-' + $window.innerWidth + 'x' + $window.innerHeight + '.json';
+        //var spriteData = JSON.parse(spriteDataFileName);
+
+        console.log($scope.spriteDataFileName);
+
+        $.ajaxSetup({async: false});
+        // Your $.getJSON() request is now synchronous...
+
+        $scope.items = [];
+        $.getJSON( $scope.spriteDataFileName, function( data ) {
+
+            console.log('back from json');
+
+            console.log(data);
+
+            $.each( data, function( key, val ) {
+                $.each(data[key], function(k,v){
+                    if (v.frame){
+                        var posInfo = {};
+                        posInfo.bp = '-' + v.frame.x + 'px -' + v.frame.y + 'px';
+                        posInfo.w = v.frame.w + 'px';
+                        posInfo.h = v.frame.h + 'px';
+                        posInfo.t = v.spriteSourceSize.y + 'px';
+                        posInfo.l = v.spriteSourceSize.x + 'px';
+                        $scope.items.push(posInfo);
+                        console.log(posInfo);
+                    }
+                });
+            });
+        });
+
+        $.ajaxSetup({async: true});
+        console.log($scope.items.length);
+
+
+/*
+
+
+        var perRowMap = {};
+        perRowMap['320x480'] = 7;
+        perRowMap['375x667'] = 5;
+
+
         var boxyArray1 = [];
 
         var noImages = 115;
-        var deviceWidth = 375;
-        var deviceHeight = 667;
-        var imagesPerRow = 5;
+        var deviceWidth = $window.innerWidth;
+        var deviceHeight = $window.innerHeight;
+        var imagesPerRow = 7;
 
         var count = 1;
         var row = 0;
@@ -70,21 +132,41 @@ courseModule
             count = count + 1;
         }
         //console.log(boxyArray1.length);
-
-        var TweenBoxy = TweenMax.to(boxyObj1, 4.2, {counter:boxyArray1.length, repeat:0, ease:SteppedEase.config(boxyArray1.length), onComplete:boxyTweenComplete, paused:true, onUpdate:boxyTweenUpdate});
+*/
+        var boxyObj1 = { counter: 0};
+        var TweenBoxy = TweenMax.to(boxyObj1, 4.2, {counter:$scope.items.length, repeat:0,
+            ease:SteppedEase.config($scope.items.length), onComplete:boxyTweenComplete, paused:true, onUpdate:boxyTweenUpdate});
 
         function boxyTweenUpdate(){
-            if (boxyObj1.counter < boxyArray1.length){
+            if (boxyObj1.counter < $scope.items.length){
                 //console.log(boxyObj1.counter);
-                TweenMax.to('.course-background', 0, {backgroundPosition:boxyArray1[Math.ceil(boxyObj1.counter)]});
+                var pi = $scope.items[Math.ceil(boxyObj1.counter)];
+                TweenMax.to('.course-background', 0, {
+                    backgroundPosition: pi.bp,
+                    top: pi.t,
+                    left: pi.l,
+                    width: pi.w,
+                    height: pi.h
+                });
+
+                // For later
+
+                //if (boxyObj1.counter > 75){
+                //    TweenBoxy.pause();
+                //}
+
             }
+
+
         }
 
         function boxyTweenComplete(){
-            //console.log('complete');
+            console.log('complete');
             //console.log(boxyArray1[boxyArray1.length-1]);
           //  TweenMax.to('.course-background', {backgroundPosition:'-2248px -4669px', immediateRender:true});
         }
+
+      //    TweenBoxy.play();
 
 
 
